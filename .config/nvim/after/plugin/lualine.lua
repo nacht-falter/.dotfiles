51a5b374466898f0b31eb568289defffb76aab95
phaeton6680 <81268159+phaeton6680@users.noreply.github.com>
2023-01-09 20:32:49 +0100

    changed lualine colors

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -10,16 +10,20 @@ local lualine_nightfly = require("lualine.themes.nightfly")
 -- new colors for theme
 local new_colors = {
     blue = "#31748f",
-    green = "#9ccfd8",
+    green = "#1bcdb8",
     violet = "#c4a7e7",
     yellow = "#f6c177",
-    black = "#26233a",
+    black = "#000000",
+    dark = "#262439",
 }
 
 -- change nightlfy theme colors
 lualine_nightfly.normal.a.bg = new_colors.blue
 lualine_nightfly.insert.a.bg = new_colors.green
 lualine_nightfly.visual.a.bg = new_colors.violet
+lualine_nightfly.normal.b.bg = new_colors.dark
+lualine_nightfly.insert.b.bg = new_colors.dark
+lualine_nightfly.visual.b.bg = new_colors.dark
 lualine_nightfly.command = {
     a = {
         gui = "bold",
