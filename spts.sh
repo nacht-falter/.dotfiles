da3a3ba869ab6c90b7d72ad9f0e710a1a8fb52f0
nacht-falter <johannes.bernet@posteo.de>
2023-08-05 23:22:58 +0200

    Add spts.sh, a wrapper script for spotify-tui

---
+++ spts.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+# A little wrapper script for spotify-tui 
+# Can be executed from command line with artist and album arguments
+# If no arguments are provided, dmenu is used to get arguments
+if [ -z "$1" ]; then
+	artist=$(echo "" | dmenu -p "Enter artist")
+	album=$(echo "" | dmenu -p "Enter album")
+else
+	artist=$1
+	album=$2
+fi
+if [ -z "$artist" ]; then
+	exit
+elif [ -z "$album" ]; then
+	spt p --name "$artist" -a
+else
+	result=$(spt s "$album" -b -f "%a:%u" --limit 50 | grep -i "$artist:")
+	artist=$(echo "$result" | grep -io "$artist")
+	uri=${result/"$artist":/}
+	spt p -u "$uri" -f "%s %a: %b - %t"
+fi
