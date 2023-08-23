1aad000aa76f84173f655598db5f257f8746248a
nacht-falter <johannes.bernet@posteo.de>
2023-08-23 14:45:36 +0200

    Only show warnings if battery is discharging

--- scripts/battery-notification.sh
+++ scripts/battery-notification.sh
@@ -5,10 +5,20 @@ export XDG_RUNTIME_DIR=/run/user/$(id -u)
 export DISPLAY=:0
 
 percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -oP 'percentage:\s*\K\d+')
-if [ "$percentage" -lt 20 ]; then
-    notify-send "Battery Warning" "Battery level low ($percentage%)." --urgency="critical"
-elif [ "$percentage" -lt 50 ]; then
-    notify-send "Battery Warning" "Battery half empty ($percentage%)." --urgency="low"
-elif [ "$percentage" == 100 ]; then
-    notify-send "Battery Warning" "Battery full ($percentage%)." --urgency="low"
+discharging=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -q 'discharging' && echo true || echo false)
+
+if [ "$discharging" == true ]; then
+	if [ "$percentage" -lt 20 ]; then
+		notify-send "Battery Warning" "Battery level: ($percentage%)." --urgency="critical"
+		echo "false" >~/.battery-notification-status
+		rm ~/.battery-notification-status
+	elif [ "$percentage" -lt 50 ]; then
+		notify-send "Battery Warning" "Battery level: ($percentage%)." --urgency="medium"
+		echo "false" >~/.battery-notification-status
+		rm ~/.battery-notification-status
+	fi
+fi
+if [ "$percentage" == 100 ] && [[ ! -f ~/.battery-notification-status ]]; then
+	notify-send "Battery Warning" "Battery charged" --urgency="low"
+	touch ~/.battery-notification-status
 fi
