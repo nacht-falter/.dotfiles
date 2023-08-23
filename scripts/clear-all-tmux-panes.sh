10549db3f322b610d7027b1ed803c5b1d919cc75
nacht-falter <johannes.bernet@posteo.de>
2023-08-23 14:45:54 +0200

    Add comments

--- scripts/clear-all-tmux-panes.sh
+++ scripts/clear-all-tmux-panes.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-
+#!/usr/bin/bash
+# Clear all tmux panes
 tmux list-windows -a -F '#{window_id}' | \
   xargs -I{} tmux list-panes -t {} -F '#{pane_id}' | \
   xargs -I{} tmux send-keys -t {} 'C-l'
