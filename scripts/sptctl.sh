85dcfdf2423116cff0c3c0c31f76c843060df697
nacht-falter <johannes.bernet@posteo.de>
2024-01-09 18:25:29 +0100

    fix broken spotify icon

--- scripts/sptctl.sh
+++ scripts/sptctl.sh
@@ -9,11 +9,11 @@ else
 fi
 
 if [[ "$1" == "next" ]] && spt pb -n; then
-	notify-send "Spotify" "Next track" -t $notification_timeout -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+	notify-send "Spotify" "Next track" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 elif [[ "$1" == "prev" ]] && spt pb -p; then
-	notify-send "Spotify" "Previous track" -t $notification_timeout -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+	notify-send "Spotify" "Previous track" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 elif [[ "$1" == "toggle" ]] && playback=$(spt pb -t -f %s); then
-	notify-send "Spotify" "Playback $playback" -t $notification_timeout -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+	notify-send "Spotify" "Playback $playback" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 elif [[ "$1" == "vol" ]] && current_volume=$(spt pb -f %v); then
 	if [[ "$2" == "up" ]]; then
 		new_volume="$((current_volume > (100 - step_size) ? 100 : current_volume + step_size))"
@@ -22,7 +22,7 @@ elif [[ "$1" == "vol" ]] && current_volume=$(spt pb -f %v); then
 		new_volume="$((current_volume < step_size ? 0 : current_volume - step_size))"
 		spt pb -v $new_volume
 	fi
-	notify-send "Spotify" "Volume: $new_volume" -t $notification_timeout -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+	notify-send "Spotify" "Volume: $new_volume" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 else
-	notify-send "Spotify" "No client connected" -t $notification_timeout -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+	notify-send "Spotify" "No client connected" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 fi
--- scripts/spts.sh
+++ scripts/spts.sh
@@ -22,32 +22,32 @@ artist=$(echo "$artist" | sed -E 's/(^| )([a-z])/\1\u\2/g')
 album=$(echo "$album" | sed -E 's/(^| )([a-z])/\1\u\2/g')
 
 if ! spt s "$artist" -a | grep "$artist"; then
-	notify-send "Spotify" "Artist $artist not found" -t "$notification_timeout" -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+	notify-send "Spotify" "Artist $artist not found" -t "$notification_timeout" -i ~/snap/spotify/spotify-icon.png
 	exit
 fi
 
 # Check if album is empty and if so, play artist top tracks
 if [ -z "$album" ]; then
 	spt p --name "$artist" -a > /dev/null 2>&1
-	notify-send "Spotify" "Playing top tracks by $artist" -t "$notification_timeout" -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+	notify-send "Spotify" "Playing top tracks by $artist" -t "$notification_timeout" -i ~/snap/spotify/spotify-icon.png
 
 
 # Otherwise, search album and filter by artist, then play it
 else
 	if ! spt s "$artist" -a | grep "$artist"; then
-		notify-send "Spotify" "Artist $artist not found" -t "$notification_timeout" -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+		notify-send "Spotify" "Artist $artist not found" -t "$notification_timeout" -i ~/snap/spotify/spotify-icon.png
 		exit
 	fi
 	response_albums=$(spt s "$album" -b -f "%a:%u" --limit 50)
 	echo "$response_albums"
 	if [ -z "$response_albums" ]; then
-		notify-send "Spotify" "Album $album not found" -t "$notification_timeout" -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+		notify-send "Spotify" "Album $album not found" -t "$notification_timeout" -i ~/snap/spotify/spotify-icon.png
 		exit
 	fi
-	filtered_albums=$(echo "$response_albums" | grep -i "$artist:")
+	filtered_albums=$(echo "$response_albums" | grep -i ~"$artist:")
 	echo "$filtered_albums"
 	if [ -z "$filtered_albums" ]; then
-		notify-send "Spotify" "Album $album by $artist not found" -t "$notification_timeout" -i /snap/spotify/67/usr/share/spotify/icons/spotify-linux-32.png
+		notify-send "Spotify" "Album $album by $artist not found" -t "$notification_timeout" -i ~/snap/spotify/spotify-icon.png
 		exit
 	fi
 	response_artist=$(echo "$response_albums" | grep -io -m 1 "$artist")
