0f90de964ed2e85976fd99186383a78a287e5b44
nacht-falter <johannes.bernet@posteo.de>
2024-01-14 21:08:43 +0100

    setup copilot

--- .config/nvim/lua/nacht-falter/plugins/nvim-cmp.lua
+++ .config/nvim/lua/nacht-falter/plugins/nvim-cmp.lua
@@ -39,16 +39,19 @@ return {
       },
       -- sources for autocompletion
       sources = cmp.config.sources {
-        { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
+        { name = "copilot", group_index = 2 }, -- copilot suggestions
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "luasnip", group_index = 2 }, -- snippets
+        { name = "buffer", group_index = 2 }, -- text in buffer
+        { name = "path", group_index = 2 }, -- file system paths
       },
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format {
-          maxwidth = 50,
+          mode = "symbol",
+          max_width = 50,
           ellipsis_char = "...",
+          symbol_map = { Copilot = "" },
         },
       },
     }
