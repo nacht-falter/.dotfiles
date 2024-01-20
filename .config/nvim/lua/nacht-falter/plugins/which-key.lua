fafd6c0d43eb5c565d5da0302e4435aec4e3b0f6
nacht-falter <johannes.bernet@posteo.de>
2024-01-20 01:37:40 +0100

    add which-key config

--- .config/nvim/lua/nacht-falter/plugins/which-key.lua
+++ .config/nvim/lua/nacht-falter/plugins/which-key.lua
@@ -6,8 +6,11 @@ return {
     vim.o.timeoutlen = 500
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    triggers_blacklist = {
+      -- list of mode / prefixes that should never be hooked by WhichKey
+      -- this is mostly relevant for keymaps that start with a native binding
+      i = { "j", "k", "<leader> f" },
+      v = { "j", "k", "<leader> f" },
+    },
   },
 }
