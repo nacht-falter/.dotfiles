57808dd3b6194d2ba2c98d8591d4600c8895b736
nacht-falter <johannes.bernet@posteo.de>
2024-01-20 01:36:21 +0100

    install fidget.nvim

---
+++ .config/nvim/lua/nacht-falter/plugins/fidget.lua
@@ -0,0 +1,10 @@
+return {
+  "j-hui/fidget.nvim",
+  opts = {
+    notification = {
+      window = {
+        winblend = 0, -- Background color opacity in the notification window
+      },
+    },
+  },
+}
