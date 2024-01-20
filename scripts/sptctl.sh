b21743cd1daea6f4b9a995b23798587619886c68
nacht-falter <johannes.bernet@posteo.de>
2024-01-20 01:45:00 +0100

    use playerctl instead of spt

--- scripts/sptctl.sh
+++ scripts/sptctl.sh
@@ -8,21 +8,23 @@ else
 	step_size=10
 fi
 
-if [[ "$1" == "next" ]] && spt pb -n; then
-	notify-send "Spotify" "Next track" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
-elif [[ "$1" == "prev" ]] && spt pb -p; then
-	notify-send "Spotify" "Previous track" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
-elif [[ "$1" == "toggle" ]] && playback=$(spt pb -t -f %s); then
-	notify-send "Spotify" "Playback $playback" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
-elif [[ "$1" == "vol" ]] && current_volume=$(spt pb -f %v); then
-	if [[ "$2" == "up" ]]; then
-		new_volume="$((current_volume > (100 - step_size) ? 100 : current_volume + step_size))"
-		spt pb -v $new_volume
-	else
-		new_volume="$((current_volume < step_size ? 0 : current_volume - step_size))"
-		spt pb -v $new_volume
+player=$(playerctl -l | grep spotify)
+if [[ "$player" ]]; then
+	if [[ "$1" == "next" ]] && playerctl -p spotify next; then
+		notify-send "Spotify" "Next track" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
+	elif [[ "$1" == "prev" ]] && playerctl -p spotify previous; then
+		notify-send "Spotify" "Previous track" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
+	elif [[ "$1" == "toggle" ]] && playback=$(playerctl -p spotify status) && playerctl -p spotify play-pause; then
+		notify-send "Spotify" "Playback $playback" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
+	elif [[ "$1" == "vol" ]] && current_volume=$(($(playerctl -p spotify volume) * 100)); then
+		if [[ "$2" == "up" ]]; then
+			new_volume="$((current_volume > (100 - step_size) ? 100 : current_volume + step_size))"
+			playerctl -p spotify volume $((new_volume / 100))
+		else
+			new_volume="$((current_volume < step_size ? 0 : current_volume - step_size))"
+		fi
+		notify-send "Spotify" "Volume: $new_volume" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 	fi
-	notify-send "Spotify" "Volume: $new_volume" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 else
 	notify-send "Spotify" "No client connected" -t $notification_timeout -i ~/snap/spotify/spotify-icon.png
 fi
